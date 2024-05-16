3f51226b546301d95b891fde133999e404100262
GarlicToT <sunrong0318@qq.com>
2024-05-16 20:43:42 +0800

    added home page

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    hostname: '127.0.0.1',
+    port: 3300,
+    // I hope to connect to a database in the future
+    // databaseUrl: '',
+    // secretKey: '',
+  };
+  // export the config
+  module.exports = config;
---
+++ router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// set routers for different pages
+router.get('/', function(req, res) {
+  res.sendFile(__dirname + '/src/html/HomePage.html');
+});
+
+// router.get('/DetailPage', function(req, res) {
+//   res.sendFile(__dirname + '/src/html/DetailPage.html');
+// });
+
+// router.get('/AboutMe', function(req, res) {
+//     res.sendFile(__dirname + '/src/html/AboutMe.html');
+// });
+
+// router.get('/ChatPage', function(req, res) {
+//     res.sendFile(__dirname + '/src/html/ChatPage.html');
+// });
+
+// export router
+module.exports = router;
